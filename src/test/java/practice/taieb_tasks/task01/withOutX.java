d47bcc158105101d07037f7938896c176d0cec10
Taieb <ouberka022@gmail.com>
2026-07-14 11:35:25 -0400

    second task

---
+++ src/test/java/practice/taieb_tasks/task01/withOutX.java
@@ -0,0 +1,14 @@
+package practice.taieb_tasks.task01;
+
+public class withOutX {
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+    public String withoutX(String str) {
+return str;
+    }
+}
