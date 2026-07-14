9abb60f355ebc6810a53a9cf70f090b972b0c32a
Taieb <ouberka022@gmail.com>
2026-07-14 11:19:56 -0400

    without2 task

---
+++ src/test/java/practice/taieb_tasks/task01/withOut2.java
@@ -0,0 +1,19 @@
+package practice.taieb_tasks.task01;
+
+public class withOut2 {
+    public static void main(String[] args) {
+        System.out.println(without2("HelloHe"));
+    }
+    public static String without2(String str) {
+        String firstTwo = str.substring(0,2);
+        String lastTwo = str.substring(str.length()-2);
+        String result = "";
+        if (firstTwo.equals(lastTwo)){
+            result = str.substring(2);
+        }else {
+            result = str;
+        }
+
+return result;
+    }
+}
