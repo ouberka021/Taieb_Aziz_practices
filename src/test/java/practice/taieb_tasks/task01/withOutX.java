0be780ba8186f6563f295cc02c30109ddd770da5
Taieb <ouberka022@gmail.com>
2026-07-14 13:22:22 -0400

    withoutx task

--- src/test/java/practice/taieb_tasks/task01/withOutX.java
+++ src/test/java/practice/taieb_tasks/task01/withOutX.java
@@ -2,13 +2,24 @@ package practice.taieb_tasks.task01;
 
 public class withOutX {
     public static void main(String[] args) {
-
+        System.out.println(without("xHxix") );
 
     }
+    public static String without(String str) {
+        String first = str.substring(0,1);
+        String last = str.substring(str.length()-1);
+        String result=str;
+
+            if (first.equals("x")){
+               result = str.replace(first,"");
+            }
+
+        if (last.equals("x")){
+            result = str.replace(last,"");
+        }
 
 
-
-    public String withoutX(String str) {
-return str;
+        return result;
     }
+
 }
