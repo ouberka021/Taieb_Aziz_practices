a91c5d4aeb46c601b050b58fdcd4c5289366d1e1
taieb <ouberka021@gmail.com>
2026-07-13 20:49:43 -0400

    first task

--- src/test/java/practice/ArrayList_Practices.java
+++ src/test/java/practice/ArrayList_Practices.java
@@ -5,5 +5,16 @@ public class ArrayList_Practices {
 
         System.out.println("Hi");
         System.out.println("Hello taib");
+        String text = "How are you!";
+        System.out.println(message(text));
+
     }
+
+
+    public static String message(String str) {
+
+        return str;
+    }
+
+
 }
