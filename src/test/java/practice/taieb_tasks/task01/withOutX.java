503e61a17ed2330712596502e13ca52946df7497
taieb <ouberka021@gmail.com>
2026-07-15 18:19:29 -0400

    first  task01 and task02

--- src/test/java/practice/taieb_tasks/task01/withOutX.java
+++ src/test/java/practice/taieb_tasks/task01/withOutX.java
@@ -2,23 +2,23 @@ package practice.taieb_tasks.task01;
 
 public class withOutX {
     public static void main(String[] args) {
-        System.out.println(without("xHxix") );
+        System.out.println(without("xHx"));
 
     }
+
     public static String without(String str) {
-        String first = str.substring(0,1);
-        String last = str.substring(str.length()-1);
-        String result=str;
-
-            if (first.equals("x")){
-               result = str.replace(first,"");
-            }
-
-        if (last.equals("x")){
-            result = str.replace(last,"");
+        String last = str.substring(0, str.length() - 1);
+        String first = str.substring(1);
+        String result;
+        if (str.startsWith("x") && str.endsWith("x") && str.length() >= 2) {
+            result = str.substring(1, str.length() - 1);
+        } else if (str.startsWith("x")) {
+            result = first;
+        } else if (str.endsWith("x")) {
+            result = last;
+        } else {
+            result = str;
         }
-
-
         return result;
     }
 
